c564c2221d582aecf098cc62c7a166f6fe610bb2
vxWrath <kylecain111@gmail.com>
2025-06-15 15:43:57 -0500

    Initial

---
+++ peerless_lib/models.py
@@ -0,0 +1,87 @@
+from typing import Any, Dict, List, Literal, Mapping, Optional, Self, Type, TypedDict
+from uuid import uuid4
+
+from discord.utils import MISSING
+from pydantic import BaseModel as PydanticBaseModel
+from pydantic import ConfigDict, Field, ModelWrapValidatorHandler, model_validator
+
+from .namespace import Namespace
+
+__all__ = (
+    'LeagueData',
+    'SettingData',
+    'TeamData',
+    'RolePing',
+    'GlobalPing',
+    'PlayerData',
+    'PlayerLeagueData',
+)
+
+type SettingType = Literal['alert', 'channel', 'day', 'number', 'option', 'ping', 'role', 'status', 'theme', 'timezone']
+
+class DataModel(PydanticBaseModel, Mapping):
+    model_config = ConfigDict(populate_by_name=True, arbitrary_types_allowed=True)
+
+    @model_validator(mode='wrap')
+    @classmethod
+    def model_validator(cls: Type[Self], data: Dict[str, Any], handler: ModelWrapValidatorHandler) -> Self:
+        for key, field in cls.model_fields.items():
+            if (val := data.get(key, MISSING)) is MISSING:
+                continue
+
+            if field.annotation and isinstance(val, dict) and Namespace in field.annotation.mro():
+                data[key] = Namespace(val)
+
+        return handler(data)
+
+    def __getattribute__(self, key: str) -> Any:
+        if (
+            key in super().__getattribute__('__pydantic_fields__').keys()
+            and key not in super().__getattribute__('__pydantic_fields_set__')
+        ):
+            raise ValueError(f"'{self.__class__.__name__}.{key}' is not currently available. Make sure you fetched it.")
+        
+        return super().__getattribute__(key)
+    
+    def __setitem__(self, key: str, val: Any) -> None:
+        setattr(self, key, val)
+
+    def __getitem__(self, key: str) -> Any:
+        return getattr(self, key)
+    
+    def __len__(self) -> int:
+        return len(self.model_dump())
+    
+class LeagueData(DataModel):
+    id: int
+    teams: Namespace[str, 'TeamData'] = Field(default_factory=Namespace)
+    settings: Namespace[str, 'SettingData[Any]'] = Field(default_factory=Namespace)
+
+class SettingData[V: Any](PydanticBaseModel):
+    value: V
+    type: SettingType
+
+class RolePing(TypedDict):
+    key: Literal["role"]
+    value: List[int]
+
+class GlobalPing(TypedDict):
+    key: Literal["everyone", "here"]
+    value: None
+
+class TeamData(PydanticBaseModel):
+    token: str = Field(default_factory=lambda : str(uuid4()))
+
+    role_name: str
+    role_id: Optional[int] = None
+    emoji_id: Optional[int] = None
+
+class PlayerData(DataModel):
+    id: int
+
+    def __getattribute__(self, key: str) -> Any:
+        return super(PydanticBaseModel, self).__getattribute__(key)
+
+class PlayerLeagueData(DataModel):
+    player_id: int
+    league_id: int
