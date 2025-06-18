9ec43b2cc56c2a2cf3562e9e0d3c759c3d296919
vxWrath <kylecain111@gmail.com>
2025-06-18 16:29:39 -0500

    Add IPC functionality and example IPC command

---
+++ peerless/ipc/test.py
@@ -0,0 +1,18 @@
+from typing import Any, Dict, Optional
+
+from pydantic import BaseModel
+
+from utility import RedisCommand, get_logger
+
+logger = get_logger()
+
+class TestMessage(BaseModel):
+    message: str
+
+class TestCommand(RedisCommand):
+    CHANNEL = 'test'
+    MODEL = TestMessage
+
+    async def handle(self, context: TestMessage) -> Optional[Dict[str, Any]]:
+        logger.info(f"Received message: {context.message}")
+        return {"status": "success"}
--- utility/__init__.py
+++ utility/__init__.py
@@ -1,6 +1,7 @@
 from .cache import *
 from .database import *
 from .env import *
+from .ipcmodels import *
 from .logger import *
 from .models import *
 from .namespace import *
